22274ad17c3a031b6f8b44153e2fd9a30266773a
Lapfips <fargier.ethan@gmail.com>
2024-12-17 10:19:46 +0100

    Testv1.1

--- src/main.sh
+++ src/main.sh
@@ -25,7 +25,7 @@ case $FUNC in
 esac
 
 if [[ -f "$FUNC" ]]; then
-    bash "RepositoryManager/$func" $2 $3 $4 $5
+    bash "RepositoryManager/$func" "$2" "$3" "$4" "$5"
 else
     echo -e "\nFunction script $FUNC not found."
     exit 1
