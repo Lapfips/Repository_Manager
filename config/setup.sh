13e3187f34160e93cd06336208d1c40936cb1f30
Lapfips <fargier.ethan@gmail.com>
2024-12-19 09:54:37 +0100

    V_4.1_Tests_logs

--- config/setup.sh
+++ config/setup.sh
@@ -14,11 +14,14 @@ if ! command -v git &> /dev/null; then
     echo -e "${YELLOW}Git is not installed. Installing Git...\n${NC}"
     sudo apt update && sudo apt install -y git || {
         echo -e "${RED}Failed to install Git. Exiting.${NC}"
+        echo -e "Failed to install Git.\n" >> ~/Repository_Manager/logs/Configuration.log
         exit 1
     }
-    echo -e "${GREEN}Git successfully installed.${NC}"
+    echo -e "${GREEN}Git successfully installed.${NC}" 
+    echo -e "Git successfully installed.\n" >> ~/Repository_Manager/logs/Configuration.log
 else
     echo -e "${GREEN}Git is already installed.${NC}"
+    echo -e "Git is already installed.\n" >> ~/Repository_Manager/logs/Configuration.log
 fi
 
 # SSH Key generation accorded to the user choice
@@ -32,9 +35,11 @@ if [[ -d "$HOME/.ssh" ]]; then
                 echo -e "${BOLD}\n$(cat $HOME/.ssh/id_ed25519.pub)${NC}"
                 echo -e "${GREEN}\nPaste this key on your GitHub profile --> https://github.com/settings/keys${NC}"
                 echo -e "${GREEN}New SSH key successfully created.${NC}"
+                echo -e "New SSH key successfully created.\n" >> ~/Repository_Manager/logs/Configuration.log
                 break ;;
             [Nn]*)
                 echo -e "${GREEN}Keeping the existing SSH key.${NC}"
+                echo -e "Keeping the existing SSH key.\n" >> ~/Repository_Manager/logs/Configuration.log
                 break ;;
             *)
                 echo -e "${RED}Please answer y or n.${NC}" ;;
@@ -46,6 +51,7 @@ else
     echo -e "${BOLD}\n$(cat $HOME/.ssh/id_ed25519.pub)${NC}"
     echo -e "${GREEN}\nPaste this key on your GitHub profile --> https://github.com/settings/keys${NC}"
     echo -e "${GREEN}New SSH key successfully created.${NC}"
+    echo -e "New SSH key successfully created.\n" >> ~/Repository_Manager/logs/Configuration.log
 fi
 
 # Configuring .gitconfig accorded to the user choice
@@ -62,9 +68,11 @@ if [[ -f "$HOME/.gitconfig" ]]; then
                 git config --global user.name "$NAME"
                 git config --global user.email "$EMAIL"
                 echo -e "${GREEN}.gitconfig has been updated successfully.${NC}"
+                echo -e ".gitconfig has been updated successfully.\n" >> ~/Repository_Manager/logs/Configuration.log
                 break ;;
             [Nn]*)
                 echo -e "${GREEN}No changes were made to your .gitconfig file.${NC}"
+                echo -e "No changes were made to .gitconfig file.\n" >> ~/Repository_Manager/logs/Configuration.log
                 break ;;
             *)
                 echo -e "${RED}Please answer y or n.${NC}" ;;
@@ -77,6 +85,7 @@ else
     git config --global user.name "$NAME"
     git config --global user.email "$EMAIL"
     echo -e "${GREEN}.gitconfig file created successfully.${NC}"
+    echo -e ".gitconfig has been updated successfully.\n" >> ~/Repository_Manager/logs/Configuration.log
 fi
 
 # Configuring and executing .bash_profile accorded to the user choice
@@ -90,9 +99,11 @@ if [[ -f "$HOME/.bash_profile" ]]; then
             [Yy]*)
                 cp ~/Repository_Manager/config/.bash_profile "$HOME/.bash_profile"
                 echo -e "${GREEN}.bash_profile has been updated successfully.${NC}"
+                echo -e ".bash_profile has been updated successfully.\n" >> ~/Repository_Manager/logs/Configuration.log
                 break ;;
             [Nn]*)
                 echo -e "${GREEN}No changes were made to your .bash_profile file.${NC}"
+                echo -e "No changes were made to .bash_profile file.\n" >> ~/Repository_Manager/logs/Configuration.log
                 break ;;
             *)
                 echo -e "${RED}Please answer y or n.${NC}" ;;
@@ -102,7 +113,10 @@ else
     echo -e "${YELLOW}Creating a new .bash_profile...${NC}"
     cp ~/Repository_Manager/config/.bash_profile "$HOME/.bash_profile"
     echo -e "${GREEN}.bash_profile file created successfully.${NC}"
+    echo -e ".bash_profile has been updated successfully.\n" >> ~/Repository_Manager/logs/Configuration.log
 fi
 
 echo -e "${GREEN}\nSetup execution completed successfully.\n
 ${NC}${BOLD}Enter ${YELLOW}'. .bash_profile'${NC}${BOLD} to initialize your profile.\n${NC}"
+
+echo -e "Setup execution completed successfully.\n" >> ~/Repository_Manager/logs/Configuration.log
--- src/.Category/.AddCategory.sh
+++ src/.Category/.AddCategory.sh
@@ -1,43 +1,45 @@
 #!/bin/bash
 
-if [[ "$1" = "-help" ]]; then
-    echo -e "\nUsage prog -cat -add <category_name> <repository_name(optionnal)>\n"
-    exit 0
+if [[ -z "$1" ]]; then
+    echo -e "\nPlease enter the following information : \n"
+    read -p "Enter the category name: " CATEGORY_NAME
 else
-    if [[ -z "$1" ]]; then
-        echo -e "\nPlease enter the following information : \n"
-        read -p "Enter the category name: " CAT
+    if [[ "$1" = "-help" ]]; then
+        echo -e "\nUsage prog -cat -add <category_name> <repository_name(optionnal)>\n"
+        exit 0
     else
         CAT="$1"
-        echo
     fi
 fi
 
-if [[ -f "Repository_Manager/src/.Update_Repositories/.Update_$CAT.sh" ]]; then
-    echo -e "\nError: The file for category '$CAT' already exist."
+if [[ -f "Repository_Manager/src/.Update_Repositories/.Update_$CATEGORY_NAME.sh" ]]; then
+    echo -e "\nError: The file for category '$CATEGORY_NAME' already exist."
     exit 1
 else
-    cp Repository_Manager/config/.Default_Update.sh "Repository_Manager/src/.Update_Repositories/.Update_$CAT.sh"
-fi
-
-if [[ "$2" == "" ]]; then
-    read -p "Do you want to add a repository to your new category -> $CAT ? (yes/no) : " USER_CHOICE
-else
-    REPOSITORY_NAME="$2"
-    echo
-    ./Repository_Manager/src/main.sh -repo -add "$CAT" "$REPOSITORY_NAME"
-    echo
-    read -p "Do you want to add another repository to your new category -> $CAT ? (yes/no) : " USER_CHOICE 
+    cp Repository_Manager/config/.Default_Update.sh "Repository_Manager/src/.Update_Repositories/.Update_$CATEGORY_NAME.sh"
 fi
 
 if [[ "$3" == "-n" ]]; then
-    USER_CHOICE="no"
+    USER_CHOICE_ADD_NEW_REPOSITORY="no"
 fi
 
-while [[ "$USER_CHOICE" != "no" ]]; do
+if [[ -z "$2" ]]; then
+    read -p "Do you want to add a repository to your new category -> $CATEGORY_NAME ? (yes/no) : " USER_CHOICE_ADD_NEW_REPOSITORY
+else
+    REPOSITORY_NAME="$2"
+    echo
+    ./Repository_Manager/src/main.sh -repo -add "$CATEGORY_NAME" "$REPOSITORY_NAME"
+    echo
+fi
+
+if [[ "$USER_CHOICE_ADD_NEW_REPOSITORY" != "no" ]]; then
+    read -p "Do you want to add another repository to your new category -> $CATEGORY_NAME ? (yes/no) : " USER_CHOICE_ADD_NEW_REPOSITORY
+fi
+
+while [[ "$USER_CHOICE_ADD_NEW_REPOSITORY" != "no" ]]; do
     echo
     read -p "Enter the repository name you want to add : " REPOSITORY_NAME
-    ./Repository_Manager/src/main.sh -repo -add "$CAT" "$REPOSITORY_NAME"
+    ./Repository_Manager/src/main.sh -repo -add "$CATEGORY_NAME" "$REPOSITORY_NAME"
     echo
-    read -p "Do you want to add another repository to your new category -> $CAT ? (yes/no) : " USER_CHOICE 
+    read -p "Do you want to add another repository to your new category -> $CATEGORY_NAME ? (yes/no) : " USER_CHOICE_ADD_NEW_REPOSITORY 
 done
