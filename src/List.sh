3941c711d302b0e63ed2fc14cc905fde732d2dff
Lapfips <fargier.ethan@gmail.com>
2024-12-19 15:01:27 +0100

    Fixing_list_pb

--- src/List.sh
+++ src/List.sh
@@ -35,7 +35,7 @@ text="Your categories and repositories structure : \n\n"
         if [[ $(basename "$cat") != ".Update_All.sh" ]]; then
             cat_basename=$(basename "$cat")
             cat=${cat_basename#".Update_"}
-            echo $cat
+            cat=${cat#".sh"}
             text+="     "$cat" : \n"
             repositories=$(sed -n 's/repositories=( //p' "Repository_Manager/src/.Update_Repositories/.Update_$cat.sh")
             for repo in $repositories; do
