e7fb730516edf074fde9483c5e57f8ce19c67bec
Ethan FARGIER <fargier.ethan@gmail.com>
2024-12-23 00:22:05 +0100

    V_5.1_Test_list_cat

--- src/.Category/.ListCategories.sh
+++ src/.Category/.ListCategories.sh
@@ -1,17 +1,5 @@
 #!/bin/bash
 
-function Count_Categories() {
-    COUNT=0
-    for cat in $(ls -a Repository_Manager/src/.Update_Repositories/.Update_*.sh); do
-        if [[ $(basename "$cat") != ".Update_All" ]]; then
-            COUNT=+1
-        fi
-    done
-    return $COUNT
-}
-
-text="Your categories structure : \n\n"
-
 if [[ "$1" != "" ]]; then
     if [[ "$1" == "-help" ]]; then
         echo -e "Usage: prog -cat -list\n"
@@ -22,12 +10,25 @@ if [[ "$1" != "" ]]; then
     fi
 fi
 
-Count=$(Count_Categories)
-if [[ $Count > 0 ]]; then
+function Count_Categories() {
+    REPOSITORIES_COUNT=0
+    for cat in $(ls -a Repository_Manager/src/.Update_Repositories/.Update_*.sh); do
+        if [[ $(basename "$cat") != ".Update_All.sh" ]]; then
+            REPOSITORIES_COUNT=1
+        fi
+    done
+}
+
+Count_Categories
+
+text="Your categories structure : \n\n"
+
+if [[ $REPOSITORIES_COUNT != 0 ]]; then
     for cat in $(ls -a Repository_Manager/src/.Update_Repositories/.Update_*.sh); do
         if [[ $(basename "$cat") != ".Update_All" ]]; then
             cat_basename=$(basename "$cat")
             cat=${cat_basename#".Update_"}
+            cat=${cat%".sh"}
             text+="     "$cat"\n"
         fi
     done
