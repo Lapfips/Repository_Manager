531f963c935d9557635c8c5132c6453530f32aaa
Ethan FARGIER <fargier.ethan@gmail.com>
2024-12-30 15:54:57 +0100

    Test_log--all_V2

--- src/Log.sh
+++ src/Log.sh
@@ -12,7 +12,7 @@ if [[ "$1" == "-help" ]]; then
 else
     if [[ "$1" == "-a" || "$1" == "--all" ]]; then
         for repo in $(ls "Repository_Manager/logs"); do
-            $(prog -log $(basename $repo))
+            ./Repository_Manager/src/Log.sh -log $(basename $repo)
         done
     else
         if [[ -z "$1" ]]; then
