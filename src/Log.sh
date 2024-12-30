cb7de20b225e9b4004a24eb13c87933ac10f1ef9
Ethan FARGIER <fargier.ethan@gmail.com>
2024-12-30 16:13:46 +0100

    Test_log--all_V9

--- src/Log.sh
+++ src/Log.sh
@@ -7,24 +7,20 @@ CYAN=$(tput setaf 6)
 BOLD=$(tput bold)
 NC=$(tput sgr0)
 
-function DISPLAY_LOG() {
-    LOG_INFO_MESSAGE="${BOLD}\nLogs from your $(basename $LOG_FILE_NAME) file :\n\n${NC}"
-    if [[ $LOG_FILE_NAME != "TRUE" ]]; then
-        while IFS= read -r line; do
-            case "$line" in
-                *"successfully"*|*"added"*) LOG_INFO_MESSAGE+="${GREEN}$line\n${NC}" ;;
-                *"failed"*|*"removed"*) LOG_INFO_MESSAGE+="${RED}$line\n${NC}" ;;
-                *) LOG_INFO_MESSAGE+="${YELLOW}$line\n${NC}" ;;
-            esac
-        done < "$LOG_FILE_NAME"
-        if [[ "$LOG_INFO_MESSAGE" == "${BOLD}\nLogs from your $(basename $LOG_FILE_NAME) file :\n\n${NC}" ]]; then
-            LOG_INFO_MESSAGE+="${RED}You don't have any log yet.\n${NC}"
-        fi
-        echo -e $LOG_INFO_MESSAGE
+if [[ "$1" == "-help" ]]; then
+    echo -e "Usage : prog -log <log_file_name>"
+else
+    if [[ -z "$1" ]]; then
+        echo -e "\nYour log files : \n"
+        for repo in $(ls "Repository_Manager/logs"); do
+            echo -e "$(basename $repo)"
+        done
+        echo
+        read -p "Enter the log file name you want to display : " LOG_FILE_NAME
     else
-        echo -e "${RED}\nToo much log file name corresponding try again.\n${NC}"
+        LOG_FILE_NAME="$1"
     fi
-}
+fi
 
 function IS_MORE_THAN_ONE_REPOSITORY_CORRESPONDING() {
     COUNT="0"
@@ -43,29 +39,22 @@ function IS_MORE_THAN_ONE_REPOSITORY_CORRESPONDING() {
     fi
 }
 
-if [[ "$1" == "-help" ]]; then
-    echo -e "Usage : prog -log <log_file_name>"
-else
-    if [[ "$1" == "-a" || "$1" == "--all" ]]; then
-        for repo in $(ls "Repository_Manager/logs"); do
-            LOG_FILE_NAME="Repository_Manager/logs/$repo"
-            DISPLAY_LOG
-        done
-    else
-        if [[ -z "$1" ]]; then
-            echo -e "\nYour log files : \n"
-            for repo in $(ls "Repository_Manager/logs/"); do
-                echo -e "$(basename $repo)"
-            done
-            echo
-            read -p "Enter the log file name you want to display : " LOG_FILE_NAME
-        else
-            LOG_FILE_NAME="$1"
-        fi
-    fi
-fi
-
 IS_MORE_THAN_ONE_REPOSITORY_CORRESPONDING
 
-DISPLAY_LOG
+LOG_INFO_MESSAGE="${BOLD}\nLogs from your $(basename $LOG_FILE_NAME) file :\n\n${NC}"
 
+if [[ $LOG_FILE_NAME != "TRUE" ]]; then
+    while IFS= read -r line; do
+        case "$line" in
+            *"successfully"*|*"added"*) LOG_INFO_MESSAGE+="${GREEN}$line\n${NC}" ;;
+            *"failed"*|*"removed"*) LOG_INFO_MESSAGE+="${RED}$line\n${NC}" ;;
+            *) LOG_INFO_MESSAGE+="${YELLOW}$line\n${NC}" ;;
+        esac
+    done < "$LOG_FILE_NAME"
+    if [[ "$LOG_INFO_MESSAGE" == "${BOLD}\nLogs from your $(basename $LOG_FILE_NAME) file :\n\n${NC}" ]]; then
+        LOG_INFO_MESSAGE+="${RED}You don't have any log yet.\n${NC}"
+    fi
+    echo -e $LOG_INFO_MESSAGE
+else
+    echo -e "${RED}\nToo much log file name corresponding try again.\n${NC}"
+fi
